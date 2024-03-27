3ba7eebccae62ebc26e0c2aa2b164adcff540924
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-03-27 21:52:37 +0530

    App: Stock Qunatity Updated

--- dist/controller/order.js
+++ dist/controller/order.js
@@ -27,20 +27,81 @@ const getOrders = (req, res, next) => __awaiter(void 0, void 0, void 0, function
     }
 });
 exports.getOrders = getOrders;
+// export const addOrder = async (req: IRequest, res: Response, next: NextFunction) => {
+//     const userId = req.user.userId
+//     try {
+//         const {
+//             shippingAddress,
+//             totalAmount,
+//             paymentId,
+//             paymentMethod,
+//             products,
+//             discount,
+//             shippingCharges,
+//         } = req.body
+//         for (const product of products) {
+//             const productId = product.productId;
+//             const productDoc = await Product.findById(productId);
+//             if (productDoc) {
+//                 product.product = productDoc._id;
+//             }
+//         }
+//         const order = new Order({
+//             userId: userId,
+//             orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true, }),
+//             orderNumber: generateOrderNumber(),
+//             shippingAddress: shippingAddress,
+//             totalAmount: totalAmount,
+//             paymentId: paymentId,
+//             paymentMethod: paymentMethod,
+//             products: products,
+//             discount: discount,
+//             shippingCharges: shippingCharges,
+//         })
+//         const tempOrder = await order.save();
+//         const orderId = tempOrder._id;
+//         const newOrder = await tempOrder.populate(['products.product', 'shippingAddress'])
+//         Object.assign(newOrder, {email : req.user.email});
+//         const mail = sendOrderConfirmationMail(newOrder)
+//         res.status(200).json({ success: true, data: newOrder, message: 'Order placed Successfully!' });
+//     } catch (error) {
+//         res.status(404).json({ success: true, data: error, message: 'Failed to place order!' });
+//     }
+// }
+const mongoose_1 = __importDefault(require("mongoose"));
 const addOrder = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const session = yield mongoose_1.default.startSession();
+    session.startTransaction();
     const userId = req.user.userId;
     try {
         const { shippingAddress, totalAmount, paymentId, paymentMethod, products, discount, shippingCharges, } = req.body;
+        // Validate request data
+        if (!shippingAddress || !totalAmount || !paymentId || !paymentMethod || !products) {
+            return res.status(400).json({ success: false, message: 'Missing required fields.' });
+        }
+        // Fetch and populate product details
         for (const product of products) {
             const productId = product.productId;
-            const productDoc = yield Product_1.default.findById(productId);
-            if (productDoc) {
-                product.product = productDoc._id;
+            const productDoc = yield Product_1.default.findById(productId).session(session);
+            if (!productDoc) {
+                yield session.abortTransaction();
+                session.endSession();
+                return res.status(404).json({ success: false, message: `Product not found for ID: ${productId}` });
             }
+            // Check product availability
+            if (product.quantity > productDoc.stockQuantity) {
+                yield session.abortTransaction();
+                session.endSession();
+                return res.status(400).json({ success: false, message: `Not enough quantity available for product ID: ${productId}` });
+            }
+            product.product = productDoc._id;
+            // Update product quantity
+            yield Product_1.default.findByIdAndUpdate(productId, { $inc: { quantity: -product.quantity } }, { session });
         }
+        // Create and save the order
         const order = new Order_model_1.default({
             userId: userId,
-            orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true, }),
+            orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true }),
             orderNumber: generateOrderNumber(),
             shippingAddress: shippingAddress,
             totalAmount: totalAmount,
@@ -50,16 +111,21 @@ const addOrder = (req, res, next) => __awaiter(void 0, void 0, void 0, function*
             discount: discount,
             shippingCharges: shippingCharges,
         });
-        const tempOrder = yield order.save();
-        const orderId = tempOrder._id;
-        const newOrder = yield tempOrder.populate(['products.product', 'shippingAddress']);
-        Object.assign(newOrder, { email: req.user.email });
-        const mail = (0, email_1.sendOrderConfirmationMail)(newOrder);
-        // startOrderStatusUpdateScheduler(orderId);
-        res.status(200).json({ success: true, data: newOrder, message: 'Order placed Successfully!' });
+        const savedOrder = yield order.save({ session });
+        // Commit the transaction
+        yield session.commitTransaction();
+        session.endSession();
+        // Populate additional order details and send confirmation email
+        const populatedOrder = yield savedOrder.populate(['products.product', 'shippingAddress']);
+        Object.assign(populatedOrder, { email: req.user.email });
+        const mail = (0, email_1.sendOrderConfirmationMail)(populatedOrder);
+        res.status(200).json({ success: true, data: populatedOrder, message: 'Order placed successfully!' });
     }
     catch (error) {
-        res.status(404).json({ success: true, data: error, message: 'Failed to place order!' });
+        console.error('Error placing order:', error);
+        yield session.abortTransaction();
+        session.endSession();
+        res.status(500).json({ success: false, message: 'Failed to place order.' });
     }
 });
 exports.addOrder = addOrder;
@@ -70,43 +136,4 @@ function generateOrderNumber() {
     const randomPart = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
     const orderNumber = `${prefix}${datePart}${randomPart}`;
     return orderNumber;
-}
-function startOrderStatusUpdateScheduler(orderId) {
-    const intervalId = setInterval(() => updateOrderStatus(orderId, intervalId), 60 * 60 * 1000);
-}
-function updateOrderStatus(orderId, intervalId) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const order = yield Order_model_1.default.findById(orderId);
-            if (!order) {
-                clearInterval(intervalId);
-                return;
-            }
-            let newStatus = order.orderStatus;
-            switch (order.orderStatus) {
-                case 'Placed':
-                    newStatus = 'In Process';
-                    break;
-                case 'In Process':
-                    newStatus = 'Dispatched';
-                    break;
-                case 'Dispatched':
-                    newStatus = 'In Transit';
-                    break;
-                case 'In Transit':
-                    newStatus = 'Delivered';
-                    break;
-                default:
-                    newStatus = order.orderStatus;
-                    break;
-            }
-            if (order.orderStatus == 'Delivered') {
-                clearInterval(intervalId);
-                return;
-            }
-            const updatedOrder = yield Order_model_1.default.updateOne({ _id: orderId }, { $set: { orderStatus: newStatus } });
-        }
-        catch (error) {
-        }
-    });
 }
--- src/controller/order.ts
+++ src/controller/order.ts
@@ -16,8 +16,58 @@ export const getOrders = async (req: IRequest, res: Response, next: NextFunction
         res.status(404).json({ success: true, data: error, message: 'Failed to fetch orders!' });
     }
 }
+// export const addOrder = async (req: IRequest, res: Response, next: NextFunction) => {
+//     const userId = req.user.userId
+//     try {
+//         const {
+//             shippingAddress,
+//             totalAmount,
+//             paymentId,
+//             paymentMethod,
+//             products,
+//             discount,
+//             shippingCharges,
+//         } = req.body
+
+//         for (const product of products) {
+//             const productId = product.productId;
+//             const productDoc = await Product.findById(productId);
+//             if (productDoc) {
+//                 product.product = productDoc._id;
+//             }
+//         }
+
+//         const order = new Order({
+//             userId: userId,
+//             orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true, }),
+//             orderNumber: generateOrderNumber(),
+//             shippingAddress: shippingAddress,
+//             totalAmount: totalAmount,
+//             paymentId: paymentId,
+//             paymentMethod: paymentMethod,
+//             products: products,
+//             discount: discount,
+//             shippingCharges: shippingCharges,
+//         })
+//         const tempOrder = await order.save();
+//         const orderId = tempOrder._id;
+//         const newOrder = await tempOrder.populate(['products.product', 'shippingAddress'])
+//         Object.assign(newOrder, {email : req.user.email});
+//         const mail = sendOrderConfirmationMail(newOrder)
+//         res.status(200).json({ success: true, data: newOrder, message: 'Order placed Successfully!' });
+//     } catch (error) {
+//         res.status(404).json({ success: true, data: error, message: 'Failed to place order!' });
+//     }
+
+// }
+
+import mongoose from 'mongoose';
+
 export const addOrder = async (req: IRequest, res: Response, next: NextFunction) => {
-    const userId = req.user.userId
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const userId = req.user.userId;
     try {
         const {
             shippingAddress,
@@ -27,19 +77,38 @@ export const addOrder = async (req: IRequest, res: Response, next: NextFunction)
             products,
             discount,
             shippingCharges,
-        } = req.body
+        } = req.body;
 
-        for (const product of products) {
-            const productId = product.productId;
-            const productDoc = await Product.findById(productId);
-            if (productDoc) {
-                product.product = productDoc._id;
-            }
+        // Validate request data
+        if (!shippingAddress || !totalAmount || !paymentId || !paymentMethod || !products) {
+            return res.status(400).json({ success: false, message: 'Missing required fields.' });
         }
 
+        // Fetch and populate product details
+        for (const product of products) {
+            const productId = product.productId;
+            const productDoc = await Product.findById(productId).session(session);
+            if (!productDoc) {
+                await session.abortTransaction();
+                session.endSession();
+                return res.status(404).json({ success: false, message: `Product not found for ID: ${productId}` });
+            }
+            // Check product availability
+            if (product.quantity > productDoc.stockQuantity) {
+                await session.abortTransaction();
+                session.endSession();
+                return res.status(400).json({ success: false, message: `Not enough quantity available for product ID: ${productId}` });
+            }
+            product.product = productDoc._id;
+
+            // Update product quantity
+            await Product.findByIdAndUpdate(productId, { $inc: { quantity: -product.quantity } }, { session });
+        }
+
+        // Create and save the order
         const order = new Order({
             userId: userId,
-            orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true, }),
+            orderDate: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true }),
             orderNumber: generateOrderNumber(),
             shippingAddress: shippingAddress,
             totalAmount: totalAmount,
@@ -48,20 +117,28 @@ export const addOrder = async (req: IRequest, res: Response, next: NextFunction)
             products: products,
             discount: discount,
             shippingCharges: shippingCharges,
-        })
-        const tempOrder = await order.save();
-        const orderId = tempOrder._id;
-        const newOrder = await tempOrder.populate(['products.product', 'shippingAddress'])
-        Object.assign(newOrder, {email : req.user.email});
+        });
 
-        const mail = sendOrderConfirmationMail(newOrder)
-        // startOrderStatusUpdateScheduler(orderId);
-        res.status(200).json({ success: true, data: newOrder, message: 'Order placed Successfully!' });
+        const savedOrder = await order.save({ session });
+
+        // Commit the transaction
+        await session.commitTransaction();
+        session.endSession();
+
+        // Populate additional order details and send confirmation email
+        const populatedOrder = await savedOrder.populate(['products.product', 'shippingAddress'])
+        Object.assign(populatedOrder, { email: req.user.email });
+        const mail = sendOrderConfirmationMail(populatedOrder);
+
+        res.status(200).json({ success: true, data: populatedOrder, message: 'Order placed successfully!' });
     } catch (error) {
-        res.status(404).json({ success: true, data: error, message: 'Failed to place order!' });
+        console.error('Error placing order:', error);
+        await session.abortTransaction();
+        session.endSession();
+        res.status(500).json({ success: false, message: 'Failed to place order.' });
     }
+};
 
-}
 
 function generateOrderNumber() {
     const prefix = 'VogueV';
@@ -70,44 +147,4 @@ function generateOrderNumber() {
     const randomPart = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
     const orderNumber = `${prefix}${datePart}${randomPart}`;
     return orderNumber;
-}
-
-function startOrderStatusUpdateScheduler(orderId: Types.ObjectId) {
-    const intervalId: any = setInterval(() => updateOrderStatus(orderId, intervalId), 60 * 60 * 1000);
-}
-
-
-
-async function updateOrderStatus(orderId: Types.ObjectId, intervalId: any) {
-    try {
-        const order = await Order.findById(orderId);
-        if (!order) {
-            clearInterval(intervalId);
-            return;
-        }
-        let newStatus = order.orderStatus;
-        switch (order.orderStatus) {
-            case 'Placed':
-                newStatus = 'In Process';
-                break;
-            case 'In Process':
-                newStatus = 'Dispatched';
-                break;
-            case 'Dispatched':
-                newStatus = 'In Transit';
-                break;
-            case 'In Transit':
-                newStatus = 'Delivered';
-                break;
-            default:
-                newStatus = order.orderStatus;
-                break;
-        }
-        if (order.orderStatus == 'Delivered') {
-            clearInterval(intervalId);
-            return;
-        }
-        const updatedOrder = await Order.updateOne({ _id: orderId }, { $set: { orderStatus: newStatus } });
-    } catch (error: any) {
-    }
 }
